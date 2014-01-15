b43ff85047994a6f0f931f727df88ed28e414e17
derek little <dlittle@WS171-MBAir.local>
2014-01-15 10:55:22 -0500

    initial commit
    *slider* version. Testing highlight effects and images
    
    Signed-off-by: derek little <dlittle@WS171-MBAir.local>

---
+++ app/scripts/main.js
@@ -0,0 +1,62 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        bootstrapAffix: '../bower_components/sass-bootstrap/js/affix',
+        bootstrapAlert: '../bower_components/sass-bootstrap/js/alert',
+        bootstrapButton: '../bower_components/sass-bootstrap/js/button',
+        bootstrapCarousel: '../bower_components/sass-bootstrap/js/carousel',
+        bootstrapCollapse: '../bower_components/sass-bootstrap/js/collapse',
+        bootstrapPopover: '../bower_components/sass-bootstrap/js/popover',
+        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/scrollspy',
+        bootstrapTab: '../bower_components/sass-bootstrap/js/tab',
+        bootstrapTooltip: '../bower_components/sass-bootstrap/js/tooltip',
+        bootstrapTransition: '../bower_components/sass-bootstrap/js/transition',
+        swiper: 'idangerous.swiper-2.1.min',
+        swiperScrollbar: 'idangerous.swiper.scrollbar-2.1'
+    },
+    shim: {
+        bootstrapAffix: {
+            deps: ['jquery']
+        },
+        bootstrapAlert: {
+            deps: ['jquery']
+        },
+        bootstrapButton: {
+            deps: ['jquery']
+        },
+        bootstrapCarousel: {
+            deps: ['jquery']
+        },
+        bootstrapCollapse: {
+            deps: ['jquery']
+        },
+        bootstrapPopover: {
+            deps: ['jquery']
+        },
+        bootstrapScrollspy: {
+            deps: ['jquery']
+        },
+        bootstrapTab: {
+            deps: ['jquery']
+        },
+        bootstrapTooltip: {
+            deps: ['jquery']
+        },
+        bootstrapTransition: {
+            deps: ['jquery']
+        },
+        swiper: {
+            deps: ['jquery']
+        },
+        swiperScrollbar: {
+            deps: ['swiper']
+        }
+    }
+});
+
+require(['app', 'swiper','swiperScrollbar','jquery'], function (app, swiper, scrollbar, $) {
+    'use strict';
+    // use app here
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
