5d5e536bd9991b8d41c3be32443fca5b6f5133df
derek little <dlittle@WS171-MBAir.local>
2014-01-30 22:52:17 -0500

    initial swiper version
    Signed-off-by: derek little <dlittle@WS171-MBAir.local>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,9 @@ require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
        // fitText: 'jquery.fittext',
-        touche: 'touche.min'
+        touche: 'touche.min',
+        swiper: 'idangerous.swiper-2.1.min',
+        swiperScrollbar: 'idangerous.swiper.scrollbar-2.1'
     },
     shim: {
         fitText: {
@@ -10,11 +12,17 @@ require.config({
         },
         touche: {
             deps: ['jquery']
+        },
+        swiper: {
+            deps: ['jquery']
+        },
+        swiperScrollbar: {
+            deps: ['swiper']
         }
     }
 });
 
-require(['simple','touche','jquery'], function (app, touche, $) {
+require(['simple','touche','swiper','swiperScrollbar', 'jquery'], function (app, touche,swiper, scrollbar, $) {
     'use strict';
     // use app here
     console.log(app);
