f63667a8e43c50a2050be50e01277a76c6b19a36
dlittle42 <dlittle42@gmail.com>
2016-02-11 23:53:01 -0500

    update info

---
+++ dist/scripts/vendor/871d9ef8.modernizr.js
@@ -0,0 +1,4 @@
+/* Modernizr (Custom Build) | MIT & BSD
+ * Build: http://modernizr.com/download/#-shiv-load-cssclasses-touch-history-csstransforms3d-opacity-csstransitions-csstransforms-json-hsla-rgba-svg-input
+ */
+;Modernizr.addTest("json",!!window.JSON&&!!JSON.parse);
